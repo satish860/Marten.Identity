6cfd4afb59795d8260fbe85ac6c82103bb0813ea
satish venkatakrishnan <svenkatakris@vmware.com>
2021-01-05 20:07:07 +0530

    Create Collection with Fluent Docker #6

---
+++ tests/Marten.Identity.Tests/MartenConnectionTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Marten;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using Xunit;
+
+namespace Marten.Identity.Tests
+{
+    public class MartenConnectionTests :DatabaseCollectionBase
+    {
+        private readonly MartenTestFixture fixture;
+
+        public MartenConnectionTests(MartenTestFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+
+        [Fact]
+        public void Should_be_able_to_Connect_to_Document_Store()
+        {
+            var store = DocumentStore.For("host=localhost;database=postgres;password=mysecretpassword;username=postgres");
+            IDocumentSession session= store.OpenSession();
+            session.Connection.FullState.Should().Be(ConnectionState.Open);
+        }
+    }
+}
---
+++ tests/Marten.Identity.Tests/MartenTestFixture.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Ductus.FluentDocker.Builders;
+using Ductus.FluentDocker.Services;
+
+namespace Marten.Identity.Tests
+{
+    public class MartenTestFixture : IDisposable
+    {
+        private IContainerService containerService;
+
+        public MartenTestFixture()
+        {
+            this.StartDocker();
+
+        }
+
+        private void StartDocker()
+        {
+            containerService = new Builder().UseContainer()
+                .UseImage("postgres")
+                .WithEnvironment("POSTGRES_PASSWORD=mysecretpassword")
+                .ReuseIfExists()
+                .WithLabel("MartenTestDB")
+                .ExposePort(5432, 5432)
+                .WaitForPort("5432/tcp", 10000 /*10s*/, "127.0.0.1")
+                .Build();
+            containerService.RemoveOnDispose = true;
+            containerService.Start();
+        }
+
+        public void Dispose()
+        {
+            containerService.Dispose();
+        }
+    }
+}
